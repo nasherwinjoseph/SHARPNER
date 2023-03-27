46f4d5378c5934a63382d9d6327fc4fd7da0ad84
SHERWIN JOSEPH <128394195+nasherwinjoseph@users.noreply.github.com>
2023-03-27 20:30:06 +0530

    Query Selector & Selector All Added

--- htmlitemlist.js
+++ htmlitemlist.js
@@ -28,3 +28,37 @@ li[1].style.backgroundcolor = 'yellow';
 for(var i=0; i< li.length; i++){
   li[1].style.backgroundcolor = '#f4f4f4';
 }
+
+
+QUERYSELECTOR
+var header = document.queySelector('main-header');
+header.style.borderBottom = 'solid 4px #ccc';
+
+var input = document.querySelector('input');
+input.value ='Hello World'
+
+var submit = document.querySelector('input[type="submit"]');
+submit.value="SEND"
+var item = document.querySelctor('.list-group-item');
+item.style.color = 'red';
+
+var lastItem = document.querySelector
+('.list-group-item.last-child');
+lastitem.style.color = 'blue';
+
+
+var secondItem document.querySelctor
+('.list-group-item:nth-child(2)');
+secondItem.style.color = 'coral';
+//QUERYSELECTRALL 
+var titles = document.querySelectorAll('.title');
+
+console.log(title);
+titles[0].textContent = 'Hello';
+
+
+
+
+
+
+
